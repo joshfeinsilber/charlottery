a7a17d0947eaffe11e50d5bf3cba681c49c11edc
Josh Feinsilber <joshfeinsilber@gmail.com>
2024-05-12 22:52:42 -0700

    starting off the game

--- src/screens/game/App.tsx
+++ src/screens/game/App.tsx
@@ -4,14 +4,25 @@ import { IKeyAction } from '../../components/keyboard/Keys'
 import { LetterQueue } from './LetterQueue'
 import { currentWord } from '../../store/game'
 import { Word } from './Word'
+import { useEffect } from 'react'
+import { lettersForToday } from '../../util/lottery/letters'
 
 export const Game = () => {
   const [word, setWord] = useAtom(currentWord)
+
+  // When the game begins, set the first letter
+  useEffect(() => {
+    setWord(lettersForToday()[0])
+  }, [])
 
   const onKeyPress = (action: IKeyAction) => {
     if (action.type === 'letter') {
       setWord(word + action.value)
     } else if (action.type === 'backspace') {
+      // Always keep the start letter
+      if (word.length <= 1) {
+        return
+      }
       setWord(word.slice(0, -1))
     }
   }
